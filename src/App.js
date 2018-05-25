80f213b202447a7454a0a9201818ea7cf436ad29
marc.llopis <marc.llopis@elementsinteractive.es>
2018-05-25 11:58:34 +0200

    Connection created to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import { database } from './firebase';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount() {
+    database.ref().on('value', () => {
+      console.log('changed DB!');
+    });
+  }
+
   render() {
     return (
       <div className="App">
