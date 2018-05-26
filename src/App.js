d74a85b918ed0f08a6a6ced11a697771896de4eb
Marc Llopis <marcllopis89@gmail.com>
2018-05-26 17:22:12 +0200

    creates a form and a child on firebase

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      newData: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
     database.ref().on('value', (snapshot) => {
       this.setState({
         data: snapshot.val(),
-      })
+      });
     });
+  }
+
+  handleChange(e) {
+    const newData = e.target.value;
+    this.setState({
+      newData,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    database.ref()
+            .child('NEWDATA')
+            .set(this.state.newData);
   }
 
   render() {
@@ -25,8 +43,16 @@ class App extends Component {
           <h2>Welcome to React and Firebase</h2>
         </div>
         <pre className="App--data">
-          { JSON.stringify(this.state.data, null, 2) }
+          { JSON.stringify(this.state.data) }
         </pre>
+        <form className="App--form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.newData}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
